9182893fb9fb35e25f295df00609a54edc70ef41
matteomara99 <114187606+matteomara99@users.noreply.github.com>
2022-10-11 23:26:46 +0200

    add php

---
+++ php/query.php
@@ -0,0 +1,7 @@
+<?php
+
+$login = "SELECT * from users where username LIKE ";
+
+$runway = "SELECT runwayId, runways.icaoCode, priRwyDir, priRwyId, priRwyElv, secRwyDir, secRwyId, secRwyElv, numberRwy FROM airports INNER JOIN runways ON airports.IcaoCode = runways.IcaoCode WHERE airports.IcaoCode LIKE '" . $_GET['icao'] . "' ORDER BY runways.numberRwy";
+
+?>
