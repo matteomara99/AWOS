0438c7073a7c0279e2f39d0378d497f1493a7b4c
matteomara99 <114187606+matteomara99@users.noreply.github.com>
2022-10-11 22:43:36 +0200

    Add files via upload

---
+++ php/php/index.php
@@ -0,0 +1,7 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<?php header('Location: login.php')?>
+	</body>
+<html>
---
+++ php/php/login.php
@@ -0,0 +1,26 @@
+<?php
+
+	include 'connection_open.php';
+	include 'query.php';
+	
+?>
+
+	<form action="login_check.php" method="post">
+
+		<div class="container">
+			<label for="uname"><b>Username</b></label>
+			<input type="text" placeholder="Enter Username" name="uname" required>
+
+			<label for="psw"><b>Password</b></label>
+			<input type="password" placeholder="Enter Password" name="psw" required>
+
+			<button type="submit">Login</button>
+		</div>
+		
+	</form>
+
+<?php
+
+	include 'connection_close.php';
+	
+?>
