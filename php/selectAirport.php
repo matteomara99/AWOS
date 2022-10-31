7cb36fe7961d1bdc639d7f9ed6a815a1b33a259b
matteomara99 <matteomarangon1999@gmail.com>
2022-10-31 14:19:06 +0100

    selectAirport

--- php/selectAirport.php
+++ php/selectAirport.php
@@ -3,7 +3,6 @@
 	include "sql\query.php";
 ?>
 
-    <body>
         <form action="awos.php" method="POST">
 			<div class="d-flex">
             <?php
@@ -12,17 +11,21 @@
 					$i = 0;
                     while($row = $result->fetch_assoc()) {
             ?>
-						<button name="airport" value="<?php echo strtoupper($row["icaoCode"]) ?>">
-							<span> <?php echo strtoupper($row["icaoCode"]) ?> </span>
+						<button name="airport" value="<?php echo strtoupper($row["icaoCode"]) ?>" width="100px" height="100px">
+							<span style="text-align: center;"> <?php echo strtoupper($row["icaoCode"]) ?> </span>
 							<img src="..\images\<?php echo $row["icaoCode"] ?>.jpg">
 						</button>
+
+						<div name="airport" value="<?php echo strtoupper($row["icaoCode"]) ?>">
+							<span style="text-align: center;"> <?php echo strtoupper($row["icaoCode"]) . " - " . $row["city"] . "/" . $row["name"] ?> </span>
+						</div>
+
 			<?php
 					}
                 }
             ?>
 			</div>
         </form>
-    </body>
 
 <?php
 	include "connection_close.php";
