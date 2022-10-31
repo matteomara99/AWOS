dec4fc827995f852b75349d554e7f80b91b86a83
matteomara99 <matteomarangon1999@gmail.com>
2022-10-31 14:19:18 +0100

    metar graphic

--- php/tabs/metarData.php
+++ php/tabs/metarData.php
@@ -3,16 +3,45 @@
 
 <div class="tab">
     <div class="d-flex">
-        <div style="width:20%;" class="border-white">A</div>
+        
+        <!-- left menu -->
+        <div class="border-white" style="width:20%;">
+            <div style="width:100%;">
+                <div style="width:100%;">
+                    <div style="text-align: center;"><small>PRESSURE</small></div>
+                    <div style="background-color: black; color: green;">1013</div>
+                </div>
+                <div class="d-flex">
+                    <div>°C</div>
+                    <div>DP</div>
+                    <div>%</div>
+                </div>
+            </div>
+            <div class="d-flex" style="width:100%;">
+                <!-- Atis Arr -->
+                <div style="padding:5px; width:45%;">
+                    <div style="color: yellow">C</div>
+                    <div><small>10:20</small></div>
+                </div>
+                <div style="padding:5px; width:10%;">&nbsp</div>
+                <!-- Atis Dep -->
+                <div style="padding:5px; width:45%;">
+                    <div style="color: yellow">C</div>
+                    <div><small>10:20</small></div>
+                </div>
+            </div>
+            <div>ATIS</div>
+            <div>Dati ATIS</div>
+        </div>
+
+        <!--right menu -->
         <div class="border-white" style="width:80%;">
         <?php
             $result = $conn->query($runway);
             if ($result->num_rows > 0) {
                 while($row = $result->fetch_assoc()) {
         ?>
-            <div class="">RWY <?php echo $row["priRwyId"] . "/" . $row["secRwyId"] ?>
-                <div>A</div>
-            </div>
+            <div class="">RWY <?php echo $row["priRwyId"] . "/" . $row["secRwyId"] ?> </div>
         <?php
                 }
             }
