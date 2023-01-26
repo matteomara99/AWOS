1e8dab552392a9df125f7a1197d825ae344cdd4b
matteomara99 <matteomarangon1999@gmail.com>
2023-01-26 15:03:56 +0100

    general

--- php/tabs/lastReports.php
+++ php/tabs/lastReports.php
@@ -8,7 +8,7 @@
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
     ?>
-                <span><?php echo $row["report"] ?></span>
+                <span><?php echo strtoupper($row["metar"]) ?></span>
     <?php
             }
         }
